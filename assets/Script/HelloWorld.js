f73c9848adbd1c90c80542ed3f5b5414d4cbb8ee
canhdao <daongoccanh@gmail.com>
2018-12-11 10:35:28 +0700

    + maps & zombie animation

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -1,6 +1,6 @@
 cc.Class({
     extends: cc.Component,
-
+/*
     properties: {
         label: {
             default: null,
@@ -9,14 +9,14 @@ cc.Class({
         // defaults, set visually when attaching this script to the Canvas
         text: 'Hello, World!'
     },
-
-    // use this for initialization
+*/
     onLoad: function () {
-        this.label.string = this.text;
+        //this.label.string = this.text;
+		cc.debug.setDisplayStats(false);
     },
 
-    // called every frame
     update: function (dt) {
 
     },
+
 });
